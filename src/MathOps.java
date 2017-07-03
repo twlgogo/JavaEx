643cda15213b47f16c9f1e62765742c5e0d9a0ae
twlgogo <twlgogo@163.com>
2017-07-03 16:06:22 +0800

    随机数生成测试

---
+++ src/MathOps.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+/**
+ * Created by ttwl on 2017/7/3.
+ */
+public class MathOps {
+    public static void main(String []arg){
+        Random random = new Random(47);
+        int a = random.nextInt(100);
+        System.out.print(a);
+    }
+}
